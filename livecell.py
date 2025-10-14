ef64e0ac6d37809e23b184e18d02dc7035999374
carlosuc3m <100329787@alumnos.uc3m.es>
2025-10-14 12:09:43 +0200

    add code to benchmark livecell dataset

---
+++ livecell.py
@@ -0,0 +1,138 @@
+import numpy as np
+import os
+import subprocess
+import random
+import tifffile
+import json
+import platform
+from pycocotools.coco import COCO
+
+def iou_diagonal_fast(gt, pred):
+    n = gt.max()
+    ious = np.empty(n)
+    for i in range(1, n+1):
+        mask = (gt == i) | (pred == i)
+        inter = np.count_nonzero((gt == i) & (pred == i))
+        ious[i-1] = inter / np.count_nonzero(mask)
+    return ious
+
+
+
+N_POINT_PROMPTS = 3
+
+SCRIPT_PATH = "scripts/livecell.py"
+
+LIVECELL_DIR = "/home/carlos/Pictures/samj_rebuttal/livecell/"
+REAL_FOLDER = "livecell_test_images"
+ANN_FILE = os.path.join(LIVECELL_DIR, "livecell_coco_test.json")
+RESULTS_PATH = os.path.join(os.getcwd(), "tmp")
+if not os.path.isdir(RESULTS_PATH):
+    os.makedirs(RESULTS_PATH)
+POINT_PROMPTS = os.path.join(LIVECELL_DIR, "point_prompts")
+if not os.path.isdir(POINT_PROMPTS):
+    os.makedirs(POINT_PROMPTS)
+
+
+
+FIJI_PATH = "/home/carlos/Desktop/Fiji.app"
+
+if platform.system() == "Linux":
+    FIJI_EXEC = "ImageJ-linux64"
+elif platform.system() == "Windows":
+    FIJI_EXEC = "fiji-windows-x64.exe"
+elif platform.system() == "Darwin":  # macOS
+    FIJI_EXEC = " Contents/MacOS/fiji-macos-x64"
+elif platform.system() == "Darwin" and ("arm64" in platform.machine() or "aarch64" in platform.machine()):  # macOS
+    FIJI_EXEC = " Contents/MacOS/fiji-macos-arm64"
+else:
+    raise RuntimeError(f"Unsupported OS: {platform.system()}")
+
+
+coco = COCO(ANN_FILE)
+
+
+f_names = []
+model_types = ["tiny", "small", "large", "eff", "effvit"]
+promtp_types = ["points", "bboxes"]
+
+n_ims = len(coco.loadImgs(coco.getImgIds()))
+scores_mat = np.zeros((n_ims, len(model_types) * len(promtp_types)), dtype="float64")
+
+
+
+for ii, coco_info in enumerate(coco.loadImgs(coco.getImgIds())):
+    f_names.append(coco_info["file_name"])
+    im = tifffile.imread(os.path.join(LIVECELL_DIR, REAL_FOLDER, coco_info["file_name"]))
+
+    H, W = coco_info["height"], coco_info["width"]
+    ann_ids = coco.getAnnIds(imgIds=[coco_info["id"]])
+    anns = coco.loadAnns(ann_ids)
+    mask = np.zeros((H, W), dtype=np.uint16)
+    for i, ann in enumerate(anns, start=1):
+        m = coco.annToMask(ann).astype(bool)  # HxW {0,1}
+        mask[m] = i
+
+    bboxes = []
+    points = []
+    #for i in range(33, 34):
+    for i in range(1, mask.max() + 1):
+        inds = np.where(mask == i)
+        bottom, top = int(inds[0].min()), int(inds[0].max())
+        left, right = int(inds[1].min()), int(inds[1].max())
+        #bboxes.append([[left, bottom, right - left, top - bottom]])
+        bboxes.extend([[left, bottom, right - left + 1, top - bottom + 1]])
+
+        point_inds = random.sample(range(inds[0].shape[0]), N_POINT_PROMPTS)
+        xs = inds[1][point_inds]
+        ys = inds[0][point_inds]
+        pps = []
+        for j in range(N_POINT_PROMPTS):
+            #pps.append([[int(xs[j]), int(ys[j])]])
+            pps.append([int(xs[j]), int(ys[j])])
+
+        #points.append([pps])
+        points.append(pps)
+    np.save(os.path.join(POINT_PROMPTS, coco_info["file_name"] + ".npy"), np.array(points))
+
+    command = [
+        os.path.join(FIJI_PATH, FIJI_EXEC),
+        "--ij2",
+        "--headless",
+        "--console",
+        "--run",
+        os.path.join(os.getcwd(), SCRIPT_PATH),
+        f"im_path=\"{os.path.join(LIVECELL_DIR, REAL_FOLDER, coco_info["file_name"])}\", bboxes=\"{json.dumps(bboxes)}\", points=\"{json.dumps(points)}\", " \
+        + f"tmp_path=\"{RESULTS_PATH}\""
+    ]
+
+
+    # Run the command
+    result = subprocess.run(command, capture_output=True, text=True)
+    if result.returncode != 0:
+        import shlex
+        print(shlex.join(command))
+        print("something happened")
+        raise Exception()
+
+    for j, model_type in enumerate(model_types):
+        for k, prompt_type in enumerate(promtp_types):
+            ious = []
+            for pn in range(1, mask.max() + 1):
+                path_to_tmp = os.path.join(RESULTS_PATH, f"pred_{model_type}_{prompt_type}_{pn - 1}.npy")
+                tmp_file = np.load(path_to_tmp).T
+                iou = iou_diagonal_fast((mask == pn) * 1, tmp_file)
+                ious.append(iou[0])
+            ious = np.array(ious)
+            scores_mat[ii, j * len(promtp_types) + k] = ious.mean()
+
+np.save("livecell.npy", scores_mat)
+
+import polars as pl
+cols = []
+for model_type in (model_types):
+    for prompt_type in (promtp_types):
+        cols.append(f"{model_type}_{prompt_type}")
+
+df = pl.DataFrame(scores_mat, schema=cols)
+df = df.with_columns(pl.Series("file_names", f_names))
+df.write_csv("livecell.csv")
---
+++ scripts/livecell.py
@@ -0,0 +1,71 @@
+#@String im_path
+#@String bboxes
+#@String points
+#@String tmp_path
+
+from io.bioimage.modelrunner.numpy import DecodeNumpy
+from ai.nets.samj.ij import SAMJ_Annotator
+from ai.nets.samj.communication.model import SAM2Tiny, SAM2Small, SAM2Large, EfficientSAM, EfficientViTSAML2
+from ai.nets.samj.annotation import Mask
+from net.imglib2 import Point
+
+from java.util import ArrayList
+from jarray import array as jarray
+from java.awt import Rectangle
+
+from ij import IJ
+from net.imglib2.img import ImagePlusAdapter
+from net.imglib2 import FinalInterval
+
+import os
+import json
+
+
+
+def to_point_prompts_java(points_py):
+    # If your data is nested like [[[[[x,y]], [[x,y]]]]], flatten it:
+    lst = ArrayList()
+    for pp in points_py:
+        lst.add(Point(pp[0], pp[1]))
+    return lst
+
+def to_rect_prompts_java(bboxes_py):
+    lst = ArrayList()
+    for bb in bboxes_py:
+        x, y, w, h = map(int, bb)
+        lst.add(FinalInterval(jarray([x, y], 'l'), jarray([w + x - 1, h + y - 1], 'l')))
+    return lst
+
+
+FILE_PATH = tmp_path
+
+## PARSE ARGS
+point_prompts = to_point_prompts_java(json.loads(points))
+rect_prompts = to_rect_prompts_java(json.loads(bboxes))
+
+
+models = [SAM2Tiny(), SAM2Small(), SAM2Large(), EfficientSAM(), EfficientViTSAML2()]
+models_str = ["tiny", "small", "large", "eff", "effvit"]
+
+wrapped = ImagePlusAdapter.wrap(IJ.openImage(im_path))
+for model, model_str in zip(models, models_str):
+
+    model.setImage(wrapped, None)
+    model.setReturnOnlyBiggest(True)
+
+    for ii, bbox in enumerate(rect_prompts):
+        segs = model.fetch2dSegmentation(bbox)
+        mask = Mask.getMask(wrapped.dimensionsAsLongArray()[0], wrapped.dimensionsAsLongArray()[1], segs)
+        name = "pred_" + model_str + "_bboxes_" + str(ii) + ".npy"
+        DecodeNumpy.saveNpy(os.path.join(FILE_PATH, name), mask)
+        
+    for ii, pp in enumerate(point_prompts):
+        point_list = ArrayList()
+        point_list.add(pp)
+        segs = model.fetch2dSegmentation(point_list, ArrayList())
+        mask = Mask.getMask(wrapped.dimensionsAsLongArray()[0], wrapped.dimensionsAsLongArray()[1], segs)
+        name = "pred_" + model_str + "_points_" + str(ii) + ".npy"
+        DecodeNumpy.saveNpy(os.path.join(FILE_PATH, name), mask)
+
+    model.closeProcess()
+    del model
